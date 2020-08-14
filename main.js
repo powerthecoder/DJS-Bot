5219425b51da302bb939d4dcdff73bd7e1a2a19a
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2020-08-13 22:02:55 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const config = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log("Bot Started");
+    console.log("------------------");
+});
+
