60f1b5b5bfc07cc8b4ddb4d259c11846e1572dec
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2020-08-15 11:26:18 -0600

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(msg, args) {
         msg.channel.send({embed: {
             title: "About",
-            description: "**Developer:** Leo Power \n**Discord:** <@255876083918831616>\n**Website:** https://powerthecoder.xyz/ \n**GitHub:** https://github.com/powerthecoder"
+            description: "**Developer:** Leo Power \n**Discord:** <@255876083918831616>\n[Developer Website](https://powerthecoder.xyz/)\n[GitHub Link](https://github.com/powerthecoder)"
         }})
     },
 };
