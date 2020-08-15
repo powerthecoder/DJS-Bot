b54e9b128f02f3ae8651adfdb2f204d0f3df5bd1
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2020-08-15 11:15:41 -0600

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -2,9 +2,9 @@ module.exports = {
     name: 'about',
     descriptiuon: 'About Bot Author',
     execute(msg, args) {
-        const embed = MessageEmbed()
-            .setTitle("About")
-            .setDescription("**Developer:** Leo Power \n**Discord:** <@255876083918831616>\n**Website:** https://powerthecoder.xyz/ \n**GitHub:** https://github.com/powerthecoder")
-        msg.channel.send(embed)
+        msg.channel.send({embed: {
+            title: "About",
+            description: "**Developer:** Leo Power \n**Discord:** <@255876083918831616>\n**Website:** https://powerthecoder.xyz/ \n**GitHub:** https://github.com/powerthecoder"
+        }})
     },
 };
